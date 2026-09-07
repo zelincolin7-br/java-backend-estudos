00e9d0e345ce5e673fb2f403447f855ae329dff1
zelincolin7-br <zelincolin7@gmail.com>
2026-09-07 15:54:03 -0300

    refactor(terraform): update SonarQube container configuration with new port and environment variables

--- terraform/sonarqube.tf
+++ terraform/sonarqube.tf
@@ -7,18 +7,21 @@ resource "docker_image" "sonarqube" {
   keep_locally = true
 }
 
-# Container do SonarQube exposto na porta 9000
+# Container do SonarQube exposto na porta 9001
 resource "docker_container" "sonarqube" {
-  name  = "sonarqube-local"
+  name  = "sonarqube-app"
   image = docker_image.sonarqube.image_id
 
   ports {
     internal = 9000
-    external = 9000
+    external = 9001
   }
 
   env = [
-    "SONAR_SEARCH_JAVAADDITIONALOPTS=-Dnode.store.allow_mmap=false"
+    "SONAR_SEARCH_JAVAADDITIONALOPTS=-Dnode.store.allow_mmap=false",
+    "SONAR_JAVA_OPTS=-Xms512m -Xmx512m",
+    "SONAR_WEB_JAVAOPTS=-Xms512m -Xmx512m",
+    "SONAR_CE_JAVAOPTS=-Xms512m -Xmx512m"
   ]
 
   restart = "unless-stopped"
