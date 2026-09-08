b66d12073f75f9eef602daa225825fb985c17af3
zelincolin7-br <zelincolin7@gmail.com>
2026-09-08 14:06:46 -0300

    feat(terraform): add Docker volumes for SonarQube data persistence and update container configuration

--- terraform/sonarqube.tf
+++ terraform/sonarqube.tf
@@ -1,13 +1,25 @@
 # Configuração do provedor Docker
 
-
 # Imagem oficial do SonarQube Community LTS
 resource "docker_image" "sonarqube" {
   name         = "sonarqube:lts-community"
   keep_locally = true
 }
 
-# Container do SonarQube exposto na porta 9001
+# Volumes do Docker para persistência de dados, extensões e logs
+resource "docker_volume" "sonarqube_data" {
+  name = "sonarqube_data"
+}
+
+resource "docker_volume" "sonarqube_extensions" {
+  name = "sonarqube_extensions"
+}
+
+resource "docker_volume" "sonarqube_logs" {
+  name = "sonarqube_logs"
+}
+
+# Container do SonarQube exposto na porta 9000
 resource "docker_container" "sonarqube" {
   name  = "sonarqube-app"
   image = docker_image.sonarqube.image_id
@@ -22,6 +34,22 @@ resource "docker_container" "sonarqube" {
     "SONAR_WEB_JAVAOPTS=-Xms512m -Xmx1024m",
     "SONAR_CE_JAVAOPTS=-Xms512m -Xmx1024m"
   ]
+
+  # Mapeamento dos volumes para salvar as configurações
+  volumes {
+    volume_name    = docker_volume.sonarqube_data.name
+    container_path = "/opt/sonarqube/data"
+  }
+
+  volumes {
+    volume_name    = docker_volume.sonarqube_extensions.name
+    container_path = "/opt/sonarqube/extensions"
+  }
+
+  volumes {
+    volume_name    = docker_volume.sonarqube_logs.name
+    container_path = "/opt/sonarqube/logs"
+  }
 
   restart = "unless-stopped"
 }
