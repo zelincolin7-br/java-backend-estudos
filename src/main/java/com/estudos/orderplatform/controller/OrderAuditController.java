646b4c80427c0511b3e03f62e8227051c0f14e16
zelincolin7-br <zelincolin7@gmail.com>
2026-09-10 18:39:21 -0300

    feat(controller): enable CORS for OrderAuditController to allow requests from localhost with credentials

--- src/main/java/com/estudos/orderplatform/controller/OrderAuditController.java
+++ src/main/java/com/estudos/orderplatform/controller/OrderAuditController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/orders/audit")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class OrderAuditController {
 
     private final OrderAuditService auditService;
