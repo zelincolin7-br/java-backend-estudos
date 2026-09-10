a3e29e082e52e9e55c83d5257f02062bf93da986
zelincolin7-br <zelincolin7@gmail.com>
2026-09-10 14:03:36 -0300

    fix: update CORS configuration in OrderController to restrict origins and allow credentials

--- src/main/java/com/estudos/orderplatform/controller/OrderController.java
+++ src/main/java/com/estudos/orderplatform/controller/OrderController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class OrderController {
 
     private final OrderService orderService;
